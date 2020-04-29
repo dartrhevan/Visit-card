cac3e518cc8eccac83c740a76ad4fb8c2b9b469f
dartrhevan <vovasatunkin@mail.ru>
2020-04-29 10:04:47 +0500

    Study page - alpha version

--- app/src/main/java/com/maynorcourse/mybusinesscard/items/StudyFragment.kt
+++ app/src/main/java/com/maynorcourse/mybusinesscard/items/StudyFragment.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 
 import com.maynorcourse.mybusinesscard.R
+import com.squareup.picasso.Picasso
 
 /**
  * A simple [Fragment] subclass.
@@ -18,7 +20,15 @@ class StudyFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_study, container, false)
+        val root = inflater.inflate(R.layout.fragment_study, container, false)
+        val inst_image = root.findViewById<ImageView>(R.id.inst_image)
+        Picasso.get()//.with(context)
+            .load("http://test.project-it.info/sites/default/files/vhod_v_zdanie_irit-rtf_urfu_-_1_1.jpg")
+            /*.placeholder(R.drawable.ic_launcher_background)
+            .error(R.drawable.ic_local_phone_black_24dp)*/
+            //.fit()
+            .into(inst_image);
+        return root
     }
 
 }
